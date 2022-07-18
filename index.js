ec315991dae447583ae58eb14040572235a0e3d4
shirke123 <pujashirke1999@gmail.com>
2022-07-18 19:05:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+  }
+  ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+  })
+  
+  // jQuery function for showing modal
+  setTimeout(function () {
+    $('#demo-modal').modal();
+  }, 500);
